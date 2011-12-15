41c396e17855710c503730920386dd68e2400eb7
Fredrik Broman <frebro@gmail.com>
2011-12-15 16:27:29 +0100

    admin ab icon

--- functions.php
+++ functions.php
@@ -43,3 +43,12 @@ function rvb_widgets_init() {
   ) );
 }
 add_action( 'widgets_init', 'rvb_widgets_init' );
+
+
+/**
+ * Add a custom logo to the admin header
+ */
+function rvb_admin_logo() {
+echo '<style type="text/css"> #wp-admin-bar-wp-logo > .ab-item .ab-icon { background: url("'.get_bloginfo('stylesheet_directory').'/images/admin-ab-icon.png") center !important; } </style>';
+}
+add_action('admin_head', 'rvb_admin_logo');
