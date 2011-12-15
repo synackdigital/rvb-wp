ee48fe7f9c18bf9357d7b927c50c088d435f0615
Fredrik Broman <frebro@gmail.com>
2011-12-15 14:22:02 +0100

    graphics and colors

---
+++ content-header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template used for displaying header content in header.php
+ *
+ * @package WordPress
+ * @subpackage RVB
+ */
+?>
+
+<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+<nav id="access" role="navigation">
+  <h1 class="assistive-text"><?php _e( 'Main menu', 'synack' ); ?></h1>
+  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
+  <div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'synack' ); ?>"><?php _e( 'Skip to primary content', 'synack' ); ?></a></div>
+  <?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
+  <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+</nav><!-- #access -->
