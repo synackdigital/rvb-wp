a1e4e4868020814e9f0c286644025665fd638271
Fredrik Broman <frebro@gmail.com>
2011-12-16 13:53:20 +0100

    custom header meta

---
+++ header-meta.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Meta data in header.php
+ * Create your own header-meta.php to override these settings with your own
+ *
+ * @package WordPress
+ * @subpackage RVB
+ */
+?>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0,minimum-scale=1.0">
