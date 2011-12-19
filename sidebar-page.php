c61f064b28ea12e4e911dd8e337486660f053b76
Fredrik Broman <frebro@gmail.com>
2011-12-19 16:11:48 +0100

    page child nav

--- sidebar-page.php
+++ sidebar-page.php
@@ -11,19 +11,24 @@
 ?>
 
 <div id="sidebar-page" class="sidebar">
+
+<?php
+  // If this page has child pages, show a navigation
+  if ( get_children( $id ) ) :
+?>
   <nav class="nav">
     <ul id="menu-childpages" class="menu">
       <?php wp_list_pages("title_li=&child_of=$id"); ?>
     </ul>
   </nav>
-
 <?php
-  // If the sidebar has no widgets, then let's bail early.
-  if ( ! is_active_sidebar( 'sidebar-page'  ) )
-    return;
-  // If we get this far, we have widgets. Let do this.
+  endif;
+
+  // If the sidebar has widgets, show a widget area
+  if ( is_active_sidebar( 'sidebar-page'  ) ) :
 ?>
   <div class="widget-area" role="complementary">
     <?php dynamic_sidebar( 'sidebar-page' ); ?>
   </div>
 </div><!-- #sidebar-page -->
+<?php endif; ?>
