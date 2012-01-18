c8dae0b11bcb5aa3ba9f7d00cddce3cda0102895
Fredrik Broman <frebro@gmail.com>
2012-01-18 18:09:27 +0100

    lots of style changes for menus and links

--- functions.php
+++ functions.php
@@ -10,6 +10,12 @@ require_once get_template_directory() . '/inc/jw_custom_posts.php';
 
 
 /**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+  $content_width = 697;
+
+/**
  * Sets up theme defaults and registers support for various WordPress features.
  */
 function rvb_setup() {
