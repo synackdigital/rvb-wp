ea32df4492442895781cba1393245a650e1f114f
Fredrik Broman <frebro@gmail.com>
2012-02-09 16:20:10 +0100

    strings

--- header-content.php
+++ header-content.php
@@ -12,9 +12,9 @@
 <?php get_search_form(); ?>
 
 <nav id="access" role="navigation">
-  <h1 class="assistive-text"><?php _e( 'Main menu', 'synack' ); ?></h1>
+  <h1 class="assistive-text"><?php _e( 'Main menu', 'rvb' ); ?></h1>
   <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
-  <div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'synack' ); ?>"><?php _e( 'Skip to primary content', 'synack' ); ?></a></div>
+  <div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'rvb' ); ?>"><?php _e( 'Skip to primary content', 'synack' ); ?></a></div>
   <?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
   <?php wp_nav_menu( array( 'theme_location' => 'primary', 'depth' => '1' ) ); ?>
 </nav><!-- #access -->
