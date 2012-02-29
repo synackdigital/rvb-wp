487634a67daa28e62a9119ad555ffbc621352088
Fredrik Broman <frebro@gmail.com>
2012-02-29 17:26:19 +0100

    content styling

--- content-slat.php
+++ content-slat.php
@@ -11,18 +11,15 @@
     <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'synack' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
       <header class="entry-header">
         <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php if ( 'post' == get_post_type() ) : ?>
+        <p class="pub-info">
+          <?php synack_posted_on(); ?>
+        </p>
+        <?php endif; // End if 'post' == get_post_type() ?>
       </header><!-- .entry-header -->
 
       <div class="entry-content">
         <?php the_excerpt(); ?>
       </div><!-- .entry-content -->
-
-      <?php if ( 'post' == get_post_type() ) : ?>
-      <footer class="entry-footer">
-        <p class="pub-info">
-          <?php synack_posted_on(); ?>
-        </p>
-      </footer><!-- #entry-footer -->
-      <?php endif; ?>
     </a>
   </article><!-- #post-<?php the_ID(); ?> -->
