07c6540a3d83e17dbec7d21a46df3df54357f6f0
Fredrik Broman <frebro@gmail.com>
2012-02-29 17:38:21 +0100

    content slats

--- content-slat.php
+++ content-slat.php
@@ -8,18 +8,18 @@
 ?>
 
   <article id="post-<?php the_ID(); ?>" <?php post_class('slat'); ?>>
-    <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'synack' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
-      <header class="entry-header">
+    <header class="entry-header">
+      <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'synack' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
         <h1 class="entry-title"><?php the_title(); ?></h1>
         <?php if ( 'post' == get_post_type() ) : ?>
-        <p class="pub-info">
-          <?php synack_posted_on(); ?>
-        </p>
-        <?php endif; // End if 'post' == get_post_type() ?>
-      </header><!-- .entry-header -->
+      </a>
+      <p class="pub-info">
+        <?php synack_posted_on(); ?>
+      </p>
+      <?php endif; // End if 'post' == get_post_type() ?>
+    </header><!-- .entry-header -->
 
-      <div class="entry-content">
-        <?php the_excerpt(); ?>
-      </div><!-- .entry-content -->
-    </a>
+    <div class="entry-content">
+      <?php the_excerpt(); ?>
+    </div><!-- .entry-content -->
   </article><!-- #post-<?php the_ID(); ?> -->
