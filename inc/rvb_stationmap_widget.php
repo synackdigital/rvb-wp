4f02c788830522570a8e4b268d203f89f6ec2e12
Fredrik Broman <frebro@gmail.com>
2012-03-29 15:02:06 +0200

    stationmap widget has configurable urls

--- inc/rvb_stationmap_widget.php
+++ inc/rvb_stationmap_widget.php
@@ -45,30 +45,59 @@ class RVB_StationMap_Widget extends WP_Widget {
   }
 
   function form($instance) {
-    // this widget is not configurable
-    echo '<em>'.__('Displays an interactive map of stations', 'rvb').'</em>';
+    $instance = wp_parse_args( (array) $instance, array( 'karlshamn_url' => '', 'kyrkhult_url' => '', 'mjallby_url' => '', 'olofstrom_url' => '', 'svangsta_url' => '', 'solvesborg_url' => '' ) );
+    $karlshamn_url = strip_tags($instance['karlshamn_url']);
+    $kyrkhult_url = strip_tags($instance['kyrkhult_url']);
+    $mjallby_url = strip_tags($instance['mjallby_url']);
+    $olofstrom_url = strip_tags($instance['olofstrom_url']);
+    $svangsta_url = strip_tags($instance['svangsta_url']);
+    $solvesborg_url = strip_tags($instance['solvesborg_url']);
+    ?>
+    <p><label for="<?php echo $this->get_field_id('karlshamn_url'); ?>">Karlshamn:
+      <input class="widefat" id="<?php echo $this->get_field_id('karlshamn_url'); ?>" name="<?php echo $this->get_field_name('karlshamn_url'); ?>" type="text" value="<?php echo attribute_escape($karlshamn_url); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('kyrkhult_url'); ?>">Kyrkhult:
+      <input class="widefat" id="<?php echo $this->get_field_id('kyrkhult_url'); ?>" name="<?php echo $this->get_field_name('kyrkhult_url'); ?>" type="text" value="<?php echo attribute_escape($kyrkhult_url); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('mjallby_url'); ?>">Mjällby:
+      <input class="widefat" id="<?php echo $this->get_field_id('mjallby_url'); ?>" name="<?php echo $this->get_field_name('mjallby_url'); ?>" type="text" value="<?php echo attribute_escape($mjallby_url); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('olofstrom_url'); ?>">Olofström:
+      <input class="widefat" id="<?php echo $this->get_field_id('olofstrom_url'); ?>" name="<?php echo $this->get_field_name('olofstrom_url'); ?>" type="text" value="<?php echo attribute_escape($olofstrom_url); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('svangsta_url'); ?>">Svängsta:
+      <input class="widefat" id="<?php echo $this->get_field_id('svangsta_url'); ?>" name="<?php echo $this->get_field_name('svangsta_url'); ?>" type="text" value="<?php echo attribute_escape($svangsta_url); ?>" /></label></p>
+    <p><label for="<?php echo $this->get_field_id('solvesborg_url'); ?>">Sölvesborg:
+      <input class="widefat" id="<?php echo $this->get_field_id('solvesborg_url'); ?>" name="<?php echo $this->get_field_name('solvesborg_url'); ?>" type="text" value="<?php echo attribute_escape($solvesborg_url); ?>" /></label></p>
+    <?php
   }
 
   function update($new_instance, $old_instance) {
-    // processes widget options to be saved
-    $instance = wp_parse_args($old_instance, $new_instance);
-      return $instance;
+    $instance = $old_instance;
+    $instance['karlshamn_url'] = strip_tags($new_instance['karlshamn_url']);
+    $instance['kyrkhult_url'] = strip_tags($new_instance['kyrkhult_url']);
+    $instance['mjallby_url'] = strip_tags($new_instance['mjallby_url']);
+    $instance['olofstrom_url'] = strip_tags($new_instance['olofstrom_url']);
+    $instance['svangsta_url'] = strip_tags($new_instance['svangsta_url']);
+    $instance['solvesborg_url'] = strip_tags($new_instance['solvesborg_url']);
+    return $instance;
   }
 
   function widget($args, $instance) {
-    // outputs the content of the widget
     extract( $args );
+    $karlshamn_url = empty($instance['karlshamn_url']) ? '#' : apply_filters('karlshamn_url', $instance['karlshamn_url']);
+    $kyrkhult_url = empty($instance['kyrkhult_url']) ? '#' : apply_filters('kyrkhult_url', $instance['kyrkhult_url']);
+    $mjallby_url = empty($instance['mjallby_url']) ? '#' : apply_filters('mjallby_url', $instance['mjallby_url']);
+    $olofstrom_url = empty($instance['olofstrom_url']) ? '#' : apply_filters('olofstrom_url', $instance['olofstrom_url']);
+    $svangsta_url = empty($instance['svangsta_url']) ? '#' : apply_filters('svangsta_url', $instance['svangsta_url']);
+    $solvesborg_url = empty($instance['solvesborg_url']) ? '#' : apply_filters('solvesborg_url', $instance['solvesborg_url']);
 
     echo $before_widget;
   ?>
   <div id="rvb-stationmap">
     <img class="map" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/stationmap.png">
-    <a class="station karlshamn" href="#" title="Karlshamn">Karlshamn</a>
-    <a class="station kyrkhult" href="#" title="Kyrkhult">Kyrkhult</a>
-    <a class="station mjallby" href="#" title="Mjällby">Mjällby</a>
-    <a class="station olofstrom" href="#" title="Olofström">Olofström</a>
-    <a class="station svangsta" href="#" title="Svängsta">Svängsta</a>
-    <a class="station solvesborg" href="#" title="Sölvesborg">Sölvesborg</a>
+    <a class="station karlshamn" href="<?php echo $karlshamn_url; ?>" title="Karlshamn">Karlshamn</a>
+    <a class="station kyrkhult" href="<?php echo $kyrkhult_url; ?>" title="Kyrkhult">Kyrkhult</a>
+    <a class="station mjallby" href="<?php echo $mjallby_url; ?>" title="Mjällby">Mjällby</a>
+    <a class="station olofstrom" href="<?php echo $olofstrom_url; ?>" title="Olofström">Olofström</a>
+    <a class="station svangsta" href="<?php echo $svangsta_url; ?>" title="Svängsta">Svängsta</a>
+    <a class="station solvesborg" href="<?php echo $solvesborg_url; ?>" title="Sölvesborg">Sölvesborg</a>
   </div>
   <?php
     echo $after_widget;
