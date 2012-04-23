d4e146ea077c9f53328482e5738778f82bb6c43b
Fredrik Broman <frebro@gmail.com>
2012-04-23 11:18:59 +0200

    user shrtcodes

--- inc/rvb_user_shortcode.php
+++ inc/rvb_user_shortcode.php
@@ -9,15 +9,17 @@ function rvb_user_shortcode( $atts ) {
 
   $user = get_userdata( $atts['id'] );
 
+  $style = ($atts['clear']) ? 'clear:'.$atts['clear'].';' : '';
+
   if ( isset($user) ) :
-    $return .= '<div class="user">';
+    $return .= '<div class="user" style="'.$style.'">';
 
     $return .= get_avatar( $user->ID, 400 );
 
     $return .= '<span class="name">';
 
-    if ( $user->roles[0] =! 'board_member' )
-      $return .= '<span class="id">'.$user->ID.'</span> ';
+    if ( $user->roles[0] == 'operational_staff' )
+      $return .= '<span class="user_login">'.$user->user_login.'</span> ';
 
     $return .= '<span class="display_name">'.$user->display_name.'</span> ';
 
@@ -31,6 +33,9 @@ function rvb_user_shortcode( $atts ) {
 
     if ( isset($user->party) && $user->roles[0] == 'board_member' )
       $return .= '<span class="party">'.$user->party.'</span> ';
+
+    if ( isset($user->phone) && $user->roles[0] == 'staff' )
+      $return .= '<span class="phone">'.$user->phone.'</span> ';
 
     if ( isset($user->email_is_public) && $user->email_is_public === 'true' )
       $return .= '<a class="email" href="mailto:'.$user->user_email.'">'.$user->user_email.'</a> ';
